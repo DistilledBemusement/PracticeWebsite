c44aaed9dc314cc9c625c6f8115d96775a9d3b14
jwhittle8 <joshwhittle@talktalk.net>
2022-02-17 15:25:52 +0000

    Initial Commit

---
+++ PracticeWebsite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PracticeWebsite.Startup))]
+namespace PracticeWebsite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
